c30389a63ed4d27c8e943e80f805bd1f0e85ea3e
xanderez17 <kevxanderez17@gmail.com>
2022-06-04 17:21:10 -0500

    Subida FrontEndJWT

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
+import {IndexComponent} from "./index/index.component";
+import {LoginComponent} from "./auth/login.component";
+import {RegistroComponent} from "./auth/registro.component";
+
+
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'lista', component: ListaProductoComponent},
+  {path: 'detalle/:id', component: DetalleProductoComponent},
+  {path: 'nuevo', component: NuevoProductoComponent},
+  {path: 'editar/:id', component: EditarProductoComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
