d2d4a6d8c326a457a23e6908e9963acd08cec639
xanderez17 <kevxanderez17@gmail.com>
2022-06-08 16:04:41 -0500

    Video 12 - Capítulo 11 Roles Validados Accesos Validados Bloqueo de Acciones por Rol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,17 @@ import { EditarProductoComponent } from './producto/editar-producto.component';
 import {IndexComponent} from "./index/index.component";
 import {LoginComponent} from "./auth/login.component";
 import {RegistroComponent} from "./auth/registro.component";
+import {ProdGuardService as guard} from "./guards/prod-guard.service";
 
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registro', component: RegistroComponent},
-  {path: 'lista', component: ListaProductoComponent},
-  {path: 'detalle/:id', component: DetalleProductoComponent},
-  {path: 'nuevo', component: NuevoProductoComponent},
-  {path: 'editar/:id', component: EditarProductoComponent},
+  {path: 'lista', component: ListaProductoComponent, canActivate: [guard], data: {expectedRol: ['admin', 'user']}},
+  {path: 'detalle/:id', component: DetalleProductoComponent, canActivate: [guard], data: {expectedRol: ['admin', 'user']}},
+  {path: 'nuevo', component: NuevoProductoComponent, canActivate: [guard], data: {expectedRol: ['admin']}},
+  {path: 'editar/:id', component: EditarProductoComponent, canActivate: [guard], data: {expectedRol: ['admin']}},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
--- src/app/producto/lista-producto.component.ts
+++ src/app/producto/lista-producto.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Producto } from '../models/producto';
 import { ProductoService } from '../service/producto.service';
 import { ToastrService } from 'ngx-toastr';
+import {TokenService} from "../service/token.service";
 
 @Component({
   selector: 'app-lista-producto',
@@ -11,14 +12,23 @@ import { ToastrService } from 'ngx-toastr';
 export class ListaProductoComponent implements OnInit {
 
   productos: Producto[] = [];
+  roles: string[];
+  isAdmin = false;
 
   constructor(
     private productoService: ProductoService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private tokenService: TokenService
     ) { }
 
   ngOnInit() {
     this.cargarProductos();
+    this.roles = this.tokenService.getAuthorities();
+    this.roles.forEach(rol =>{
+      if(rol === 'ROLE_ADMIN'){
+        this.isAdmin = true;
+      }
+    });
   }
 
   cargarProductos(): void {
